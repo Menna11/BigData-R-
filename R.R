487ff2da76b10b353d6ad0779169fa713fd9035a
Menna11 <63121607+Menna11@users.noreply.github.com>
2023-05-21 12:53:06 +0300

    Update R.R

--- R.R
+++ R.R
@@ -1,6 +1,6 @@
 hotel_bookings<-read.csv("D:\\Downloads\\database\\hotel_bookings.csv")
 data<-na.omit(hotel_bookings)
-hist(data$arrival_date_week_number,main="Arrival Date Week Number Histogram",col="lightblue",xlab="Week Number",breaks = 10)
+
 #Distribution of Arrival week number using histogram
 hist(data$arrival_date_week_number,main="Arrival Date Week Number Histogram",col="lightblue",xlab="Week Number",breaks = 10)
 
